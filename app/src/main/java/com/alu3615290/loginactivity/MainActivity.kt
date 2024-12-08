87d7459525a2c51032f713f02a00d15b10f0cd04
luisc3615290 <3615290@alu.murciaeduca.es>
2024-12-08 21:58:11 +0100

    Versión Inicial 1.0 - Sólo MainActivity.kt - UI Completa - Funcionan los botones - Funcionan los snackbars

---
+++ app/src/main/java/com/alu3615290/loginactivity/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.alu3615290.loginactivity
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
+import androidx.core.view.isEmpty
+import androidx.core.view.isNotEmpty
+import androidx.core.view.size
+import com.alu3615290.loginactivity.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //básicos del binding para empezar app
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Logos de facebook y google en los botones
+        val facebookButton = binding.sesionFacebook;
+        val googleButton = binding.sesionGoogle;
+        facebookButton.setCompoundDrawablesRelativeWithIntrinsicBounds(
+            null,
+            null,
+            ContextCompat.getDrawable(getApplicationContext(), R.drawable.facebooklogo),
+            null
+        )
+        googleButton.setCompoundDrawablesRelativeWithIntrinsicBounds(
+            null,
+            null,
+            ContextCompat.getDrawable(getApplicationContext(), R.drawable.googlelogo),
+            null
+        )
+
+        //Gestión de los errores
+        binding.inicioSesion.setOnClickListener() {
+            binding.inicioSesion.setOnClickListener() {
+                Snackbar.make(it, getString(R.string.snackbarInicioSesion), Snackbar.LENGTH_INDEFINITE)
+                    .setAction(getString(R.string.snackbarAcceder)) {
+
+                    }
+                    .show()
+
+            }
+        }
+
+
+        //Gestión de los snackbars desde botones
+        binding.sesionFacebook.setOnClickListener() {
+            Snackbar.make(it, getString(R.string.snackbarFacebook), Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(R.string.snackbarAcceder)) {
+
+                }
+                .show()
+        }
+        binding.sesionGoogle.setOnClickListener() {
+            Snackbar.make(it, getString(R.string.snackbarGoogle), Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(R.string.snackbarAcceder)) {
+
+                }
+                .show()
+        }
+        binding.registro.setOnClickListener() {
+            Snackbar.make(it, getString(R.string.snackbarRegistro), Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(R.string.snackbarCerrar)) {
+
+                }
+                .show()
+        }
+        binding.paswordReset.setOnClickListener() {
+            Snackbar.make(it, getString(R.string.snackbarPasswordReset), Snackbar.LENGTH_INDEFINITE)
+                .setAction(getString(R.string.snackbarCerrar)) {
+
+                }
+                .show()
+        }
+
+    }
+
+}
